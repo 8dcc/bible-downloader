d23fa2c12cf116aab1466ad94c44f916a5b7d99a
8dcc <8dcc.git@gmail.com>
2023-02-18 17:24:32 +0100

    Add markdown version

--- print-chapters-md.py
+++ print-chapters-md.py
@@ -8,26 +8,21 @@ except Exception:
     exit(1)
 
 ##########################
-DEBUG       = False      # Print extra information.
-RECURSE     = True       # Will go to the next chapter until finished
-LINE_LIMIT  = 85         # Will jump to the next line if too long. 0 means disabled.
-LINE_INDENT = 6          # Start of text lines.
-NUM_PAD     = 5          # Padding for the numbers.
-VERSION     = "dhh"      # Bible version.
-FIRST_BOOK  = "genesis"  # Only used for replacing START_URL
-FIRST_CHAP  = "1"        #
+RECURSE      = True       # Will go to the next chapter until finished
+UPPER_TITLES = False      # Chapter titles in uppercase
+LINE_LIMIT   = 0          # Will jump to the next line if too long. 0 means disabled.
+VERSION      = "dhh"      # Bible version.
+FIRST_BOOK   = "genesis"  # Only used for replacing START_URL
+FIRST_CHAP   = "1"        #
 ##########################
 
 BASE_URL  = "https://www.biblia.es/"
 START_URL = BASE_URL + f"biblia-buscar-libros-1.php?libro={FIRST_BOOK}&capitulo={FIRST_CHAP}&version={VERSION}"
 
 # For similar C version with comments, see 8dcc/text-formatter
-# String is the string to print, limit is the max position in a line, cur_indent is
-# the current indentation of the text to be printed.
-def print_limited(string, limit, cur_indent):
+# String is the string to print, limit is the max position in a line
+def print_limited(string, limit):
     if (string == None):
-        if (DEBUG):
-            sys.stderr.write("print_limited: Got None string")
         return
 
     # For printing the last word. Space and not newline because we also want to check
@@ -39,21 +34,17 @@ def print_limited(string, limit, cur_indent):
     last_word  = ""
     for c in string:
         if (c == ' '):
-            if line_p <= limit - cur_indent:
+            if line_p <= limit:
                 if (last_space != ''):
                     sys.stdout.write(last_space)
 
                 if (last_space == '\n'):
-                    line_p = cur_indent + len(last_word)
+                    line_p = len(last_word)
             else:
                 if (last_space != ''):
                     sys.stdout.write('\n')
 
-                    # Indent line
-                    for i in range(cur_indent):
-                        sys.stdout.write(' ')
-
-                line_p = cur_indent + len(last_word)
+                line_p = len(last_word)
 
             # Print last word and clear buf
             sys.stdout.write(last_word)
@@ -78,10 +69,6 @@ def print_limited(string, limit, cur_indent):
 def main():
     url = START_URL
     while (url != ""):
-        # Extra spaces so line is clean when returning
-        if (DEBUG):
-            print("Getting: " + url + "                   \r")
-
         r = requests.get(url,
                          allow_redirects=True,
                          headers = {
@@ -108,43 +95,35 @@ def main():
                 continue
 
             if (item.get("class")[0] == "capitulo"):
-                sys.stdout.write('\n========================================\n')
-                print(item.string.upper().center(40))
-                sys.stdout.write('========================================\n\n')
+                sys.stdout.write("### ")
+                
+                if (UPPER_TITLES):
+                    print(item.string.upper())
+                else:
+                    print(item.string)
             elif (item.get("class")[0] == "estudio"):
-                if (last_printed != "capitulo"):
-                    sys.stdout.write('\n')
-
-                print(item.string)
+                sys.stdout.write('\n')
+                print("**" + item.string + "**")
                 sys.stdout.write('\n')
             elif (item.get("class")[0] == "versiculo"):
-                sys.stdout.write(item.string.rjust(NUM_PAD))
-
-                for i in range(LINE_INDENT - NUM_PAD):
-                    sys.stdout.write(' ')
+                sys.stdout.write("<sup>")
+                sys.stdout.write(item.string)
+                sys.stdout.write("</sup> ")
             elif (item.get("class")[0] == "texto"):
                 # In theory we just printed the number
                 for s in item.strings:
                     final_str = str(s).replace(chr(0x2014), "- ").replace("\r\n", "")
 
-                    if (last_printed == "texto"):
-                        final_str = "".rjust(LINE_INDENT) + final_str
-
+                    # Line limit only makes it visual
                     if (LINE_LIMIT == 0):
                         # No line limit, just print
                         print(final_str)
                     else:
                         # Line limit, jump to the next line if the line is too long
-                        print_limited(final_str, LINE_LIMIT, LINE_INDENT)
+                        print_limited(final_str, LINE_LIMIT)
 
+                    sys.stdout.write('\n')      # 2nd newline for markdown
                     last_printed = "texto"
-            elif (DEBUG):
-                sys.stdout.write(item.name)
-                sys.stdout.write('\t')
-                sys.stdout.write(item.get("class")[0])
-                sys.stdout.write('\t')
-                sys.stdout.write(item.string)
-                sys.stdout.write('\n')
 
             last_printed = item.get("class")[0]
 
